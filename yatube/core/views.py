b8da4480cef64ab5a5151d78ff083592dc720aad
Павел Чупров <p.tchuprow@yandex.ru>
2022-07-06 00:32:40 +0300

    Финал.Часть 1.1

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -5,5 +5,8 @@ def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
-def csrf_failure(request, reason=''):
+def csrf_failure(request):
     return render(request, 'core/403csrf.html')
+
+def server_error(request):
+    return render(request, 'core/500.html')
